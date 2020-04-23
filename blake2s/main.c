487d0c10a6e21a9fa16a13fcc6b892adb6098b6a
Mike <tankf33der@disroot.org>
2020-04-24 00:47:31 +0300

    .

--- blake2s/main.c
+++ blake2s/main.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <string.h>
-#include "blake2-int.h"
+#include "blake2.h"
 
 int main(void)
 {
@@ -20,6 +20,12 @@ int main(void)
 
     for (size_t i = 0; i < 256; i++) {
         blake2s(r, m, k, 64, i, 64);
+    }
+    for (size_t i = 0; i < 64; i++) {
+        blake2s(r, m, k, 64, 64, i);
+    }
+    for (size_t i = 0; i < 64; i++) {
+        blake2s(r, m, k, i, 64, 64);
     }
     return 0;
 }
