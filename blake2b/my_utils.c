1adaee9876e4be0e668165c5321e2112280cdcb6
Mike <mpech@tuta.io>
2020-04-24 12:50:32 +0300

    .

---
+++ blake2b/my_utils.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+
+void *
+memset(void *__s, int __c, size_t __n)
+{
+
+    if (__n != 0) {
+        char *d = __s;
+
+        do {
+            *d++ = __c;
+        } while (--__n != 0);
+    }
+    return (__s);
+}
+void *memcpy(void *__restrict __dest, const void *__restrict __src, size_t __n)
+{
+   char *csrc = (char *)__src;
+   char *cdest = (char *)__dest;
+   for (size_t i=0; i<__n; i++)
+       cdest[i] = csrc[i];
+}
--- blake2s/main.c
+++ blake2s/main.c
@@ -8,24 +8,24 @@
 int main(void)
 {
     printf("start\n");
-    uint8_t r[64];
-    uint8_t k[64];
+    uint8_t r[32];
+    uint8_t k[32];
     uint8_t m[256];
     for (size_t i = 0; i < 256; i++) {
         m[i] = i;
     }
-    for (size_t i = 0; i < 64; i++) {
+    for (size_t i = 0; i < 32; i++) {
         k[i] = i;
     }
 
     for (size_t i = 0; i < 256; i++) {
-        blake2s(r, m, k, 64, i, 64);
+        blake2s(r, m, k, 32, i, 32);
     }
-    for (size_t i = 0; i < 64; i++) {
-        blake2s(r, m, k, 64, 64, i);
+    for (size_t i = 0; i < 32; i++) {
+        blake2s(r, m, k, 32, 32, i);
     }
-    for (size_t i = 0; i < 64; i++) {
-        blake2s(r, m, k, i, 64, 64);
+    for (size_t i = 0; i < 32; i++) {
+        blake2s(r, m, k, i, 32, 32);
     }
     return 0;
 }
